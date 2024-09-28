66aa116ac599b189e7581a60daefdc52e93d6f26
Hovsep <hovsephakobyan04@gmail.com>
2024-09-28 22:10:21 +0400

    Add project files.

---
+++ Stack/Program.cs
@@ -0,0 +1,51 @@
+﻿Stack<int> stack = new(3);
+
+stack.Push(0);
+stack.Push(1);
+stack.Push(2);
+
+stack.Pop();
+stack.Pop();
+stack.Pop();
+
+public class Stack<T>
+{
+    public int top { get; private set; }
+    public T[] stack { get; private set; }
+
+    private int size;
+    public Stack(int size)
+    {
+        this.size = size;
+        stack = new T[size];
+        top = -1;
+    }
+
+    public bool IsEmpty()
+    {
+        return top < 0;
+    }
+
+    public T Pop()
+    {
+        if (top == -1)
+        {
+            Console.WriteLine("Stack Underflow.");
+            return default(T);
+        }
+
+        return stack[top--];
+    }
+
+    public bool Push(T value)
+    {
+        if (top == size - 1)
+        {
+            Console.WriteLine("Stack Overflow.");
+            return false;
+        }
+
+        stack[++top] = value;
+        return true;
+    }
+}
